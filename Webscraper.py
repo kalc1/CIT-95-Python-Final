72b4cbd0bc5586d0e0408382e0e8896e3fe66a54
kalc1 <kalcocer1@gmail.com>
2023-11-20 19:27:10 -0800

    Create main.txt file to store html Scrape and store html file in main.txt

--- Webscraper.py
+++ Webscraper.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = ""
+url = "https://www.yelp.com/search?find_desc=Tacos&find_loc=Fresno%2C%20CA&sortby=rating&start=0"
 
 result = requests.get(url)
-soup = BeautifulSoup(result, 'html.parser')
+soup = BeautifulSoup(result.text, 'html.parser')
 
-main_file = open('main.txt', 'w')
-main_file = write(str(soup.prettify()))
+with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\main.txt', 'w', encoding='utf-8') as main_file:
+    main_file.write(str(soup.prettify()))

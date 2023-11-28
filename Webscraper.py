4ab3470437591a6889f57dc3749d07940d37d3a4
kalc1 <kalcocer1@gmail.com>
2023-11-27 21:59:18 -0800

    use .findall to find the 10 search result urls from main.txt

--- Webscraper.py
+++ Webscraper.py
@@ -1,10 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.yelp.com/search?find_desc=Tacos&find_loc=Fresno%2C%20CA&sortby=rating&start=0"
+# The following retrieves HTML data from the first page of yelp search results for "Tacos"
+# This section is run only once to retrieve the HTML and saved to a .txt file and 
+# commented out to prevent making unnecessary requests to the website. 
 
-result = requests.get(url)
-soup = BeautifulSoup(result.text, 'html.parser')
+# # url = "https://www.yelp.com/search?find_desc=Tacos&find_loc=Fresno%2C%20CA&sortby=rating&start=0"
+# result = requests.get(url)
+# soup = BeautifulSoup(result.text, 'html.parser')
+# with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\main.txt', 'w', encoding='utf-8') as main_file:
+#     main_file.write(str(soup.prettify()))
+    
+with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\main.txt', 'r', encoding='utf-8') as m1:
+    main_1 = m1.read()
 
-with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\main.txt', 'w', encoding='utf-8') as main_file:
-    main_file.write(str(soup.prettify()))
+
+# print(main_1)     # This serves only to verify my main_1 functions as intended.
+
+soup = BeautifulSoup(main_1, 'html.parser')
+
+
+
+links = soup.find_all("a", class_ = "css-1hqkluu") # This finds the 10 search result urls in the main.txt file. I have found the first one for reference: <a class="css-1hqkluu" href="/biz/tacos-brunos-fresno?osq=Tacos" rel="noopener" target="_blank">
+
+print(links)
+        
+

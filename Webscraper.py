95f2cc80981bbcbf1bb612cb1dd338e73d63f8a6
kalc1 <kalcocer1@gmail.com>
2023-11-30 15:44:01 -0800

    create a file named links.txt and write the extracted taco links into links.txt

--- Webscraper.py
+++ Webscraper.py
@@ -14,10 +14,12 @@ os.makedirs(output_directory, exist_ok=True) # Since I have to run this a few ti
 # soup = BeautifulSoup(result.text, 'html.parser')
 # with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\yelp_html\\main.txt', 'w', encoding='utf-8') as main_file:
 #     main_file.write(str(soup.prettify()))
-    
-with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\yelp_html\\main.txt', 'r', encoding='utf-8') as m1:
-    main_1 = m1.read()
 
+try:    # Exception Handling will let me know if the file doesn't exist. This issue kept happening for some reason so this will let me know immediately if it happens again. 
+    with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\yelp_html\\main.txt', 'r', encoding='utf-8') as m1:
+        main_1 = m1.read()
+except FileNotFoundError:
+    print("File not found: c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\yelp_html\\main.txt")
 
 # print(main_1)     # This serves only to verify my main_1 functions as intended.
 
@@ -27,10 +29,9 @@ links = soup.find_all("a", class_ = "css-1hqkluu") # This finds the 10 search re
 
 # print(links) # This just serves to show what I have extracted from the soup.find_all function above
 
-for link in links:  # This extracts all the links to the taco places in the first page search results from the main url
-    taco_place = 'https://www.yelp.com' + link['href']
-    print(taco_place)
-
-
+with open('c:\\Users\\kalco\\Coding Projects\\Python\\CIT-95-Python-Final\\yelp_html\\links.txt', 'w', encoding='utf-8') as links_file:
+    for link in links:  # This extracts all the links to the taco places in the first page search results from the main url
+        taco_place = 'https://www.yelp.com' + link['href']
+        links_file.write(f'{taco_place}\n')
         
 
